079b4377a18b228cfa7b87997c7e348f77bb0c30
huskyhoochu <dfg1499@gmail.com>
2019-01-11 13:44:52 +0900

    [#2] script refactor

--- script/add_issue_number.sh
+++ script/add_issue_number.sh
@@ -4,7 +4,7 @@ COMMIT_MSG_FILE=$1
 
 branch_name=`git rev-parse --abbrev-ref HEAD`
 issue_number=`echo ${branch_name} | cut -d '-' -f1`
-first_line = `head -n1 ${COMMIT_MSG_FILE}`
+first_line=`head -n1 ${COMMIT_MSG_FILE}`
 
 # if COMMIT_MSG_FILE's first_line is empth,
 # add issue number in commit_msg
